247259a8aad0d333f5b5427934dd67bdced3483f
jose <j.ramos.olivares@gmail.com>
2020-07-29 20:16:50 -0400

    last changes

--- desafio_normalizacion.sql
+++ desafio_normalizacion.sql
@@ -1,6 +1,7 @@
-CREATE TABLE autor(id_autor SERIAL PRIMARY KEY, nombre_autor VARCHAR(50) NOT NULL);
+CREATE TABLE autor(id_autor SERIAL PRIMARY KEY, nombre_autor VARCHAR(50) NOT NULL, primer_apellido VARCHAR(50) NOT NULL, segundo_apellido VARCHAR(50));
 CREATE TABLE editorial(id_editorial SERIAL PRIMARY KEY, nombre_editorial VARCHAR(50) NOT NULL);
-CREATE TABLE cliente(id_cliente SERIAL PRIMARY KEY, nombre_cliente VARCHAR(50) NOT NULL);
+CREATE TABLE cliente(id_cliente SERIAL PRIMARY KEY, nombre_cliente VARCHAR(50) NOT NULL, ap_paterno VARCHAR(50) NOT NULL, ap_materno VARCHAR(50));
 CREATE TABLE libro(codigo_libro INT PRIMARY KEY, titulo VARCHAR(50) NOT NULL, id_editorial INT NOT NULL REFERENCES editorial (id_editorial));
 CREATE TABLE prestamo(id_cliente INT NOT NULL REFERENCES cliente (id_cliente), codigo_libro INT NOT NULL REFERENCES libro (codigo_libro), fecha_devolucion DATE NOT NULL);
 CREATE TABLE autor_libro(id_autor INT NOT NULL REFERENCES autor (id_autor), codigo_libro INT NOT NULL REFERENCES libro (codigo_libro));
+CREATE TABLE libro_editorial(codigo_libro INT NOT NULL REFERENCES libro (codigo_libro), id_editorial INT NOT NULL REFERENCES editorial (id_editorial));
